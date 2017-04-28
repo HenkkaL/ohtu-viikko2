5bec66b1be78d1aff7d64b0fd820959e53e46bf2
Henrik Lyra <henriklyra@gmail.com>
2017-04-28 10:05:45 +0300

    tehtävä 3

--- QueryLanguage/src/main/java/statistics/Main.java
+++ QueryLanguage/src/main/java/statistics/Main.java
@@ -5,7 +5,7 @@ import statistics.matcher.*;
 public class Main {
     public static void main(String[] args) {
         Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
-          
+        System.out.println("And + HasAtLeast:\n");  
         Matcher m = new And( new HasAtLeast(10, "goals"),
                              new HasAtLeast(10, "assists"),
                              new PlaysIn("PHI")
@@ -14,5 +14,36 @@ public class Main {
         for (Player player : stats.matches(m)) {
             System.out.println( player );
         }
+        
+        System.out.println("\n\nAnd + hasFewerThan:\n");
+        
+        Matcher m2 = new And( new HasFewerThan(20, "goals"),
+                             new HasFewerThan(20, "assists"),
+                             new PlaysIn("PHI")
+        );
+        
+        for (Player player : stats.matches(m2)) {
+            System.out.println( player );
+        }
+        
+        System.out.println("\n\nOr + hasAtleast:\n");
+        
+        Matcher m3 = new And(new Or( new HasAtLeast(20, "goals"),
+                             new HasAtLeast(20, "assists")),
+                             new PlaysIn("PHI"));
+        
+        for (Player player : stats.matches(m3)) {
+            System.out.println( player );
+        }        
+        
+        System.out.println("\n\nNot + hasAtleast:\n");
+        
+        Matcher m4 = new And(new Or( new HasAtLeast(25, "goals"),
+                             new HasAtLeast(30, "assists")),
+                            new Not(new PlaysIn("PHI")));
+        
+        for (Player player : stats.matches(m4)) {
+            System.out.println( player );
+        }                
     }
 }
